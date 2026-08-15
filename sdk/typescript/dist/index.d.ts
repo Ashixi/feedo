1224e808707f289b1daa96533e6af1fae5af3342
Andrii Shumko <andriishumko@gmail.com>
2026-08-15 16:03:19 +0200

    chore: untrack build artifacts, runtime DBs and node_modules; expand .gitignore

--- sdk/typescript/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { FeedoClient } from './client';
-export { FeedoNetworkConfig, NodeRouter } from './router';
-export { SearchModule } from './modules/search';
-export { ConsensusModule } from './modules/consensus';
-export { StorageModule } from './modules/storage';
-export { FeedoCrypto } from './modules/crypto';
--- sdk/typescript/dist/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { FeedoClient } from './client';
-export { FeedoNetworkConfig, NodeRouter } from './router';
-export { SearchModule } from './modules/search';
-export { ConsensusModule } from './modules/consensus';
-export { StorageModule } from './modules/storage';
